07b7fd0f306b74bd48be3809f42131f589691026
John <benjamin.dada@fcmb.com>
2023-07-04 22:57:51 +0100

    commit

---
+++ DelegateConsole/Program.cs
@@ -0,0 +1,47 @@
+﻿
+namespace DelegateConsole
+{
+    public class Program
+    {
+        delegate void LogDel(string text);
+
+        static void Main(string[] args)
+        {
+            Log log = new Log();
+            // LogDel logD = new LogDel(log.LogTextToFile);
+
+            LogDel LogTextToScreenDel, LogTextToFileDel;
+
+            LogTextToScreenDel = new LogDel(log.LogTextToScreen);
+            LogTextToFileDel = new LogDel(log.LogTextToFile);
+
+            LogDel multipleLog = LogTextToScreenDel + LogTextToFileDel;
+
+            Console.WriteLine("Please enter your name");
+            var name = Console.ReadLine();
+
+            //multipleLog(name!);
+            LogText(log.LogTextToFile, name);
+            Console.ReadKey();
+        }
+        static void LogText(LogDel logdel, string text)
+        {
+            logdel(text);
+        }
+    }
+    public class Log
+    {
+        public void LogTextToScreen(string text)
+        {
+            Console.WriteLine($"{DateTime.Now}: {text}");
+        }
+
+        public void LogTextToFile(string text)
+        {
+            using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
+            {
+                sw.WriteLine($"{DateTime.Now}: {text}");
+            }
+        }
+    }
+}
